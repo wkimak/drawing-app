f85fdeb07a520986f3016100ceaff6e6d927e167
William Kimak <william.kimak@iqvia.com>
2025-04-09 22:23:43 -0400

    add xcodeprojs

---
+++ drawing-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  drawing-app
+//
+//  Created by William Kimak on 4/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ drawing-app/drawing_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawing_appApp.swift
+//  drawing-app
+//
+//  Created by William Kimak on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct drawing_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
